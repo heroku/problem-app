40129e5a59579cdb6f7ff943c60a78f0ff9ffa87
Brent Gardner <bgardner@squarelabs.net>
2017-03-23 15:19:34 -0600

    Add some end points

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/wait', function(req, res, next) {
   setTimeout(() => {
     res.render('index', { title: 'Express' });
   }, 500);
+});
+
+router.get('/cpu', function(req, res, next) {
+  const start = new Date().getTime();
+  while(new Date().getTime() < start + 500);
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
