c0ea25bdcbdf7eb49728c42bf76c15db8f59455a
Brent Gardner <bgardner@squarelabs.net>
2017-03-23 14:38:28 -0600

    Add new relic

---
+++ newrelic.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  app_name: ['problem-app'],
+  license_key: 'a4933a5c71c722e81637607957d65e86526b99ba',
+  logging: {
+    level: 'info'
+  }
+}
